d429c15a7f44e5092a9e09189a20d7eef3271430
Priya Joshi <priya.apr9@gmail.com>
2019-09-14 14:49:47 +0530

    redux components added

--- src/components/Courses/coursePage.js
+++ src/components/Courses/coursePage.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react';
- 
+import {PropTypes} from 'prop-types';
+import {connect} from 'react-redux';
+import * as courseAction from '../../actions/courseAction';
 
 class CoursesPage extends Component{
     constructor(props){
@@ -21,18 +23,18 @@ class CoursesPage extends Component{
     onClickSave(){
          let msg="saving Data";
          alert(msg += ' '+`${this.state.course.title}`);
-      
-         
+         this.props.createCourse(this.state.course);
     }
 
-  
- 
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>
+    }
 
     render(){
         return(
             <div>
                 <h1>Courses</h1>
-               
+                {this.props.courses.map(this.courseRow)}
                 <h2>Add Courses</h2>
 
                 <input type="text" onChange={this.onTitleChange}
@@ -43,8 +45,22 @@ class CoursesPage extends Component{
         );
     }
 }
- 
 
- 
+CoursesPage.propTypes = {
+    createCourse: PropTypes.func.isRequired,
+    courses: PropTypes.array.isRequired
+}
 
-export default CoursesPage;
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        createCourse: course => dispatch(courseAction.createCourse(course))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import {Provider} from 'react-redux';
 
- 
 import App from './App';
+import configureStore from './store/configureStore';
  
-
- 
-
- 
-
+const store=configureStore();
 
 // const lp=({
 //     ProfileId,
@@ -39,4 +36,8 @@ import App from './App';
 // );
 
 //trace("initial render",performance.now(),()=>
-render(<App />,document.getElementById('root'));    
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,document.getElementById('root')
+);    
